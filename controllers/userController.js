cc66db708a13ea318effcc4a54669d4e0bc45025
Анударь Алтангэрэл <HP230043@G303-CC0156.local>
2024-01-30 20:23:26 +0800

    signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,34 @@
 const UserModel = require("../models/userModel");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 const signupUser = async (req, res) => {
   const body = req.body;
-  // const password = body.password;
-  // const hashedPassword = bcrypt.hashSync(password, 1);
-  // const data = { ...body, password: hashedPassword };
+  const password = body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const data = { ...body, password: hashedPassword };
   try {
-    await UserModel.create(body);
+    await UserModel.create(data);
     res.status(200).send({ message: "created" });
   } catch (err) {
     res.status(500).send(err);
   }
 };
 
-// const signinUser = async (req, res) => {
+const signinUser = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const user = await UserModel.findOne({ email: email });
+    const hashedPassword = user.password;
+    const isUser = bcrypt.compareSync(password, hashedPassword);
+    if (isUser) {
+      res.status(200).send({ message: "amjilttai nevterle" });
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-// }
-
-module.exports = { signupUser };
+module.exports = { signupUser, signinUser };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,17 +1,24 @@
 const { Router } = require("express");
-// const UserSchema = require("../models/userModel");
-const {signupUser} = require('../controllers/userController')
+const UserModel = require("../models/userModel");
+const {signupUser, signinUser} = require('../controllers/userController')
 const userRouter = Router();
 
-// const validateInput = () => {
-
+// const validateInput = async() => {
+//     const body = req.body
+//     const user = await UserModel.findOne({email: body.email})
+//     if(user) {
+//         res.status(403).send({message: "email address is already used"})
+//     } else{
+//         next()
+//     }
 // }
+
 
 // const validatePassword = () => {
 
 // }
 
 userRouter.post("/signup", signupUser);
-// router.post("signin", validatePassword, signinUser);
+userRouter.post("/signin", signinUser);
 
 module.exports = userRouter;
